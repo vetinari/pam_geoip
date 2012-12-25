02f3931ea8f8d70d1da72896aa5be95f32943361
vetinari <vetinari@068bfac3-6a03-4691-8544-4025894bee76>
2012-12-25 13:26:09 +0000

    * silence test
    git-svn-id: https://svn.ankh-morp.org:8081/pam_geoip/trunk@29 068bfac3-6a03-4691-8544-4025894bee76

--- make_pam_geoip_h.sh
+++ make_pam_geoip_h.sh
@@ -2,7 +2,7 @@
 
 echo "#include <GeoIP.h>
 int main () { int have = GEOIP_CITY_EDITION_REV1_V6; }" > version.c
-gcc -lGeoIP version.c -shared -o version
+gcc -lGeoIP version.c -shared -o version 2> /dev/null
 if [ $? -eq 0 ]; then
 	echo "#define HAVE_GEOIP_010408" > pam_geoip.h
 else
