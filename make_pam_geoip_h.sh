b0cbd58259992b6e13836b059b9afe56fd77009f
vetinari <vetinari@068bfac3-6a03-4691-8544-4025894bee76>
2012-12-26 12:38:54 +0000

    * use tempfile(1)
    git-svn-id: https://svn.ankh-morp.org:8081/pam_geoip/trunk@35 068bfac3-6a03-4691-8544-4025894bee76

--- make_pam_geoip_h.sh
+++ make_pam_geoip_h.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-echo "#include <GeoIP.h>
-int main () { int have = GEOIP_CITY_EDITION_REV1_V6; }" > version.c
-gcc -lGeoIP version.c -shared -o version 2> /dev/null
+TEMP_SRC=$( tempfile -s .c )
+TEMP_OUT=$( tempfile )
+
+cat > $TEMP_SRC <<_END
+#include <GeoIP.h>
+int main () {
+    int have = GEOIP_CITY_EDITION_REV1_V6;
+}
+_END
+
+gcc -lGeoIP $TEMP_SRC -shared -o $TEMP_OUT 2> /dev/null
 if [ $? -eq 0 ]; then
-	echo "#define HAVE_GEOIP_010408" > pam_geoip.h
+    rm -f pam_geoip.h
+    echo "#define HAVE_GEOIP_010408" > pam_geoip.h
 else
-	cat > pam_geoip.h <<_END
+    rm -f pam_geoip.h
+    cat > pam_geoip.h <<_END
 #ifdef HAVE_GEOIP_010408
 #undef HAVE_GEOIP_010408
 #endif
 _END
 fi
-rm -f version.c version
+rm -f $TEMP_SRC $TEMP_OUT
